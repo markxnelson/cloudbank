5a5aa7f28f8f8ff0249d8a5bf90ad62f94d1a8ab
Mark Nelson <mark.x.nelson@oracle.com>
2022-06-28 18:30:38 -0400

    fixed up account list with numbers, type, balance all coming from parse
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

--- components/Accounts/Accounts.js
+++ components/Accounts/Accounts.js
@@ -30,6 +30,19 @@ const getAccountType = async (parseAddress, accountNum) => {
     return accountType;
 }
 
+const getAccountBalance = async (parseAddress, accountNum) => {
+    console.log("mark in getAccountBalance, parseAdress = " + parseAddress + " and accountNum = " + accountNum);
+    if (parseAddress.length < 1) { return }
+    const Parse = require('parse/react-native.js');
+    Parse.setAsyncStorage(AsyncStorage);
+    Parse.initialize("APPLICATION_ID");
+    Parse.serverURL = 'http://' + parseAddress + ':1337/parse';
+    const params = { "accountNum": accountNum };
+    const balance = await Parse.Cloud.run("balance", params);
+    console.log("balance is " + balance);
+    return balance;
+}
+
 const Accounts = (props) => {
     const [parseAddress, setParseAddress] = useState("");
     const [accounts, setAccounts] = useState([]);
@@ -38,30 +51,42 @@ const Accounts = (props) => {
         console.log("getting accounts for user " + props.user)
         AsyncStorage.getItem('serverAddress')
         .then(address => {
-            setParseAddress(address) // this is a nasty hack
+            setParseAddress(address); // this is a nasty hack
             getAccounts(address, props.user)
-            .then(result => {
-                setAccounts(result);
-                //console.log("useEffect result = " + JSON.stringify(result));
+            .then(accountNumbers => {
+                //setAccounts(result);
+                // TODO - we could iterate through them here and get the type and balance,
+                //        and store these in a local state object, not just a list of numbers ... 
+                accountNumbers.forEach(item => {
+                    console.log("processing " + item);
+                    getAccountType(address, item)
+                    .then(type => {
+                        getAccountBalance(address, item)
+                        .then(balance => {
+                            // TODO - need to check for duplicate account numbers!!!
+                            setAccounts((prev) => [...prev,
+                                {
+                                    accountNumber: item,
+                                    accountType: type,
+                                    balance: formatCurrency({amount : (+balance).toFixed(2), code: 'USD' })[0]
+                                }]
+                            )
+                        })
+                    });
+                })
             });
-            // getAccountType(address, route.params.accountNumber)
-            // .then(result => setAccountType(result));
         })
         .catch(error => console.log(error))
     }, [props.user])
 
     const accountList = accounts.length !== 0 ? accounts
         .map(account => {
-            console.log("calling getAccountType");
-            const accountType = getAccountType(parseAddress, account);
-            console.log("mark - accountType = " + accountType.result);
-
             return (
-                <TouchableWithoutFeedback onPress={() => props.navigation.navigate('AccountDetail', { accountNumber: account })}>
-                    <View style={styles.row} key={account}>
-                        <View style={styles.cell}><Text>{account}</Text></View>
-                        <View style={styles.cell}><Text>accountType</Text></View>
-                        <View style={styles.cell}><Text style={styles.numbers}>$1,234.56</Text></View>
+                <TouchableWithoutFeedback onPress={() => props.navigation.navigate('AccountDetail', { accountNumber: account.accountNumber })}>
+                    <View style={styles.row} key={account.accountNumber}>
+                        <View style={styles.cell}><Text>{account.accountNumber}</Text></View>
+                        <View style={styles.cell}><Text>{account.accountType}</Text></View>
+                        <View style={styles.cell}><Text style={styles.numbers}>{account.balance}</Text></View>
                     </View>
                 </TouchableWithoutFeedback>
             )
