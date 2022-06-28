7fea41fd6833084d3dcabcbac9f9abff4023092c
Mark Nelson <mark.x.nelson@oracle.com>
2022-06-28 11:21:41 -0400

    get transcation history - hardcoded to acct 10000 for now
    Signed-off-by: Mark Nelson <mark.x.nelson@oracle.com>

--- components/Accounts/AccountDetail.js
+++ components/Accounts/AccountDetail.js
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import Card from '../UI/Card';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { formatCurrency } from "react-native-format-currency";
+
+const getHistory = async (accountNum) => {
+    const Parse = require('parse/react-native.js');
+    Parse.setAsyncStorage(AsyncStorage);
+    Parse.initialize("APPLICATION_ID");
+    Parse.serverURL = 'http://193.122.128.128:1337/parse';
+    const params = { "accountNum": accountNum };
+    const history = await Parse.Cloud.run("history", params);
+    //console.log("cloud code result= " + JSON.stringify(history));
+    return history;
+}
 
 const AccountDetail = (props) => {
+    const [history, setHistory] = useState([]);
+    useEffect(() => {
+        getHistory(10000)
+        .then(result => setHistory(result));
+        console.log("result = " + JSON.stringify(history));
+    }, [props.accountNum])
+
+    const transactions = 
+        history.length !== 0 ? history.map(transaction => {
+            const t = JSON.parse(JSON.stringify(transaction));
+            var date = new Date(t.createdAt).toDateString();
+            var amount = formatCurrency({amount : (+t.amount).toFixed(2), code: 'USD' })[0];
+            return (
+                <View style={styles.row}>
+                    <View style={styles.cell}><Text>{date}</Text></View>
+                    <View style={styles.cell}><Text>{t.action}</Text></View>
+                    <View style={styles.cell}><Text style={styles.numbers}>{amount}</Text></View>
+                </View>
+            )
+        }) : (
+            <View style={styles.row}>
+                <View style={styles.cell}>
+                    <Text>{" "}</Text>
+                    <Text>No transactions to display</Text>
+                </View>
+            </View>
+        )
+
     return (
         <ScrollView style={styles.main}>
             <Card title={"My Checking Account : " + props.accountNumber}>
-                <View style={styles.row}>
-                    <View style={styles.cell}><Text>6/27/2022</Text></View>
-                    <View style={styles.widecell}><Text>Deposit</Text></View>
-                    <View style={styles.cell}><Text style={styles.numbers}>$100.00</Text></View>
-                </View>
-                <View style={styles.row}>
-                    <View style={styles.cell}><Text>6/27/2022</Text></View>
-                    <View style={styles.widecell}><Text>Transfer to account 123</Text></View>
-                    <View style={styles.cell}><Text style={styles.numbers}>$50.00</Text></View>
-                </View>
+                {transactions}
             </Card>
         </ScrollView>
     )
